0f976eee0f5b5fdf555edc80fdb252b5ae63bacc
Antonio Crispin <antonio.crispin@gmail.com>
2019-11-29 17:21:27 -0500

    first commit

---
+++ src/main/java/com/unicon/api/serviceapicliente/ServiceApiClienteApplication.java
@@ -0,0 +1,13 @@
+package com.unicon.api.serviceapicliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApiClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceApiClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicon/api/serviceapicliente/ServiceApiClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicon.api.serviceapicliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApiClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
