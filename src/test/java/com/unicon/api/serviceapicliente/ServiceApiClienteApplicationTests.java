0e8a17cae31d404168f714d2770f626eb0d31bb3
Antonio Crispin <antonio.crispin@gmail.com>
2019-12-05 19:25:37 -0500

    lombok and test

--- src/test/java/com/unicon/api/serviceapicliente/ServiceApiClienteApplicationTests.java
+++ src/test/java/com/unicon/api/serviceapicliente/ServiceApiClienteApplicationTests.java
@@ -1,13 +1,34 @@
 package com.unicon.api.serviceapicliente;
 
+import com.unicon.api.serviceapicommons.dao.DaoApplication;
+import com.unicon.api.serviceapicommons.dao.mapper.AppMapper;
+import lombok.extern.log4j.Log4j2;
+import org.apache.ibatis.exceptions.PersistenceException;
+import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Log4j2
 class ServiceApiClienteApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("test -----------");
+		String server = "", username = "", sessionId = "", enviroment = "";
+		try (SqlSession session = DaoApplication.getSqlSessionFactory2().openSession(true)) {
+			AppMapper mapper = session.getMapper(AppMapper.class);
+			server = mapper.selectServer();
+			username = mapper.selectUsername();
+			sessionId = mapper.selectSessionId();
+			enviroment = session.getConfiguration().getEnvironment().getId();
+		} catch (PersistenceException | NullPointerException ex) {
+			log.error(ex.getMessage(), ex);
+		}
+		log.info("server: " + server);
+		log.info("username: " + username);
+		log.info("sessionId: " + sessionId);
+		log.info("enviroment: " + enviroment);
 	}
 
 }
